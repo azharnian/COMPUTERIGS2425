2e87778db9c967de06cd04104f16bcf443f3e916
Anas Azhar <anas.sty@gmail.com>
2025-05-03 09:22:33 +0700

    02-05-2025

---
+++ 11/CODING-B/REACT/07/pop-movie/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { OMDB_API_KEY } from "./config";
+
+function App() {
+  const [query, setQuery] = useState("oppenheimer");
+  const [movies, setMovies] = useState([]);
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchMovie() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?s=${query}&apikey=${OMDB_API_KEY}`
+          );
+
+          const data = await res.json();
+          console.log(data.Search);
+          setMovies(data.Search);
+          setIsLoading(false);
+        } catch (err) {
+          console.log(err);
+        } 
+    }
+
+    if (query.length < 3) {
+      setMovies([]);
+      return;
+    }
+
+    fetchMovie();
+  }, [query]);
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ 11/CODING-B/REACT/07/pop-movie/src/config.js
@@ -0,0 +1,3 @@
+const OMDB_API_KEY = import.meta.env.VITE_OMDB_API;
+
+export { OMDB_API_KEY };
